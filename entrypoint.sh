32c8d9c146deb1264b7cc07a8ec732c65e96fd5b
Marius Benthin <marius.benthin@nextron-systems.com>
2025-11-12 10:24:34 +0100

    chore: rename parameter --thor-binary to  --thor-location

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,7 +13,7 @@ exec "$TARGET_DIR/tools/thunderstorm" \
     "--queue-warn-size" "${QUEUE_WARN_SIZE:-50000}" \
     "--result-cache-size" "${RESULT_CACHE_SIZE:-250000}" \
     "--store-samples-score" "${STORE_SAMPLES_SCORE:-200}" \
-    "--thor-binary" "${THOR_BINARY:-$TARGET_DIR/thor-linux-64}" \
+    "--thor-location" "${THOR_LOCATION:-$TARGET_DIR}" \
     "--upload-dir" "$UPLOAD_DIR" \
     "--vfs-dir" "$VFS_DIR" \
     "--" \
