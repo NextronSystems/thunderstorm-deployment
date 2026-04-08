9a7e276900a32fbf752ab79f46f8862dc653bf3e
Marius Benthin <marius.benthin@nextron-systems.com>
2026-04-08 15:44:11 +0200

    fix: optional arguments

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,20 +3,24 @@
 # update THOR signatures
 "$TARGET_DIR/thor-util" update
 
+# build optional TLS and VFS arguments
+EXTRA_ARGS=""
+[ -n "$TLS_CERT" ] && EXTRA_ARGS="$EXTRA_ARGS --cert $TLS_CERT"
+[ -n "$TLS_KEY" ]  && EXTRA_ARGS="$EXTRA_ARGS --key $TLS_KEY"
+[ -n "$VFS_DIR" ]  && EXTRA_ARGS="$EXTRA_ARGS --vfs-dir $VFS_DIR"
+
 # run Thunderstorm service
 exec "$TARGET_DIR/tools/thunderstorm" \
     "--host" "${HOST:-0.0.0.0}" \
     "--port" "${PORT:-8000}" \
-    "--cert" "$TLS_CERT" \
-    "--key" "$TLS_KEY" \
     "--queue-storage" "${QUEUE_STORAGE:-$TEMP_DIR/.persisted-uploads}" \
     "--queue-warn-size" "${QUEUE_WARN_SIZE:-50000}" \
     "--result-cache-size" "${RESULT_CACHE_SIZE:-250000}" \
     "--store-samples-score" "${STORE_SAMPLES_SCORE:-200}" \
     "--thor-location" "${THOR_LOCATION:-$TARGET_DIR}" \
     "--upload-dir" "$UPLOAD_DIR" \
-    "--vfs-dir" "$VFS_DIR" \
     "--signature-update-interval" "$SIGNATURE_UPDATE_INTERVAL" \
+    $EXTRA_ARGS \
     "--" \
     "--no-json" \
     "--template" "$TARGET_DIR/config/custom-thor.yml"
